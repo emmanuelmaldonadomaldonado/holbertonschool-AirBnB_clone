6ab80f76b711b07db6ed893e7e853eb83cea1efc
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-02-25 19:44:48 -0400

    updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
@@ -24,18 +25,6 @@ class FileStorage:
         serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
-
-    """def reload(self):
-        from models.base_model import BaseModel
-        try:
-            with open(self.__file_path, "r") as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    cls = BaseModel
-                    obj = cls(**value)
-                    self.__objects[key] = obj
-        except FileNotFoundError:
-            pass"""
     
     def reload(self):
         """
