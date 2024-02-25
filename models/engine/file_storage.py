de1bbf564f641dcf9cc091c743067b10ad664d5f
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-02-25 17:18:53 -0400

    new version of file storage for user

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
     __file_path = "file.json"
@@ -19,7 +25,7 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
 
-    def reload(self):
+    """def reload(self):
         from models.base_model import BaseModel
         try:
             with open(self.__file_path, "r") as file:
@@ -28,5 +34,27 @@ class FileStorage:
                     cls = BaseModel
                     obj = cls(**value)
                     self.__objects[key] = obj
+        except FileNotFoundError:
+            pass"""
+    
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        """
+        data_dict = {}
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
+        try:
+            with open(self.__file_path, 'r') as f:
+                data_dict = json.load(f)
+                for k, v in data_dict.items():
+                    self.__objects[k] = classes[v["__class__"]](**v)
         except FileNotFoundError:
             pass
